3c1613fdd37bf9c9cc576b710d8f078f884c4d3b
ts4labels <cinnalattesims@gmail.com>
2024-11-21 14:52:01 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,17 @@
 import { Label } from './types.js';
 
 export const DELETE = '3lbi7wljayk2a';  // Updated rkey for delete
-export const LABEL_LIMIT = 1;  // Updated to allow 1 labels
+export const LABEL_LIMIT = 1;  // Updated to allow 1 label
 export const LABELS: Label[] = [
   {
     rkey: '3lbi4dxzaic2b',  // rkey for maxis_match
     identifier: 'TS4 Maxis Match',
     locales: [
-        { lang: 'en', name: 'TS4 Maxis Match', description: 'TS4 Creators of Maxis Match'},
       { 
-
+        lang: 'en', 
+        name: 'TS4 Maxis Match', 
+        description: 'TS4 Creators of Maxis Match' 
+      }
     ]
-  },
+  }
+];
