0c14697d2f4c992a360d000460366b8cc17dea8f
ts4labels <cinnalattesims@gmail.com>
2024-11-21 16:05:44 -0500

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 
-export const HANDLE = "@github-labeler.bsky.social";
-export const DID = (process.env.DID ?? "") as `did:${string}`;
+export const HANDLE = "@ts4labels.bsky.social";
+export const DID = (process.env.did:plc:fynsptksllcid25rrzrupspi"") as `did:plc:fynsptksllcid25rrzrupspi:${string}`;
 export const SIGNING_KEY = process.env.SIGNING_KEY ?? "";
 export const PORT = Number(process.env.PORT ?? 4001);
 export const MAXLABELS = 4;
