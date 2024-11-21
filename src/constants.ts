30a6ac1341aada12e31f14183a2a42421832cdeb
ts4labels <cinnalattesims@gmail.com>
2024-11-21 13:34:01 -0500

    Add files via upload

---
+++ src/constants.ts
@@ -0,0 +1,71 @@
+import { Label } from './types.js';
+
+export const DELETE = 'insert-rkey-of-delete-post-here';
+export const LABEL_LIMIT = 1;
+
+export const LABELS: Label[] = [
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'builder',
+    locales: [
+      { lang: 'en', name: 'Builder 🏡', description: 'For Sims 4 Builders' },
+      { lang: 'pt-BR', name: 'Construtor 🏡', description: 'Para Construtores do Sims 4' },
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'maxis_match',
+    locales: [
+      { lang: 'en', name: 'Maxis Match 🎮', description: 'For Maxis Match CC Creators' },
+      { lang: 'pt-BR', name: 'Maxis Match 🎮', description: 'Para Criadores de CC Maxis Match' },
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'cc_creator',
+    locales: [
+      { lang: 'en', name: 'CC Creator 🧵', description: 'For Custom Content Creators' },
+      { lang: 'pt-BR', name: 'Criador de CC 🧵', description: 'Para Criadores de Conteúdo Personalizado' },
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'pose_creator',
+    locales: [
+      { lang: 'en', name: 'Pose Creator 🤳', description: 'For Sims 4 Pose Creators' },
+      { lang: 'pt-BR', name: 'Criador de Poses 🤳', description: 'Para Criadores de Poses do Sims 4' },
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'modder',
+    locales: [
+      { lang: 'en', name: 'Modder ⚙️', description: 'For Sims 4 Modders' },
+      { lang: 'pt-BR', name: 'Modder ⚙️', description: 'Para Modders do Sims 4' },
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'storyteller',
+    locales: [
+      { lang: 'en', name: 'Storyteller 📖', description: 'For Sims 4 Storytellers' },
+      { lang: 'pt-BR', name: 'Narrador 📖', description: 'Para Contadores de Histórias do Sims 4' },
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'photographer',
+    locales: [
+      { lang: 'en', name: 'Photographer 📸', description: 'For Sims 4 Photographers' },
+      { lang: 'pt-BR', name: 'Fotógrafo 📸', description: 'Para Fotógrafos do Sims 4' },
+    ]
+  },
+  {
+    rkey: 'insert-rkey-here',
+    identifier: 'decorator',
+    locales: [
+      { lang: 'en', name: 'Decorator 🛋️', description: 'For Sims 4 Decorators' },
+      { lang: 'pt-BR', name: 'Decorador 🛋️', description: 'Para Decoradores do Sims 4' },
+    ]
+  }
+];
