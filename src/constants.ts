015b15cd334b7b2e475832378429acba4fde551b
ts4labels <cinnalattesims@gmail.com>
2024-11-24 14:47:12 -0500

    Delete src/constants.ts

--- src/constants.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-
-export const HANDLE = "@ts4labels.bsky.social";  // Change this if you're using a different handle
-export const DID = (process.env.DID ?? "") as `did:${string}`;  // Make sure the DID is set in your .env file
-export const SIGNING_KEY = process.env.SIGNING_KEY ?? "";  // Ensure this is set if required for signing
-export const PORT = Number(process.env.PORT ?? 4001);  // Change this if you need a different port
